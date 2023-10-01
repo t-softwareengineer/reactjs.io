2b3e4819473443d42f38bbf319ad1a074054f69c
t-softwareengineer <t.softwareengineer@hotmail.com>
2023-10-02 00:55:40 +0100

    build: my first react component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.scss';
 
+// My First Component
+function LogoReact() {
+  return (<img src={logo} alt="Logo" />);
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>React.js Logo's</h1>
+        <LogoReact />
+        <LogoReact />
+        <LogoReact />
+      </div>
+    </>
   );
 }
 
